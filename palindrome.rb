6707f5347a64883ff7faca92be1f51e63b0c3963
CW <compiledwrong+github@gmail.com>
2020-09-29 21:17:00 -0700

    got distracted

---
+++ palindrome.rb
@@ -0,0 +1,11 @@
+# @param {Integer} x
+# @return {Boolean}
+def is_palindrome(x)
+	x_s = s.to_s
+	leading_idx = 0
+	ending_idx = x_s.length - 1
+    while leading_idx < ending_idx do 
+    	if 
+    	
+    end
+end
