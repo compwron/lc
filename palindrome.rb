7f1095f06f3fb369c85e1625c9a2e690e93ddeb9
CW <compiledwrong+github@gmail.com>
2020-09-29 21:32:14 -0700

    ugh, ruby makes it too easy, this is not actual prep

--- palindrome.rb
+++ palindrome.rb
@@ -1,11 +1,9 @@
 # @param {Integer} x
 # @return {Boolean}
 def is_palindrome(x)
-	x_s = s.to_s
-	leading_idx = 0
-	ending_idx = x_s.length - 1
-    while leading_idx < ending_idx do 
-    	if 
-    	
-    end
+	return x.to_s == x.to_s.reverse
 end
+
+p is_palindrome(121)
+p is_palindrome(-121)
+p is_palindrome(10)
